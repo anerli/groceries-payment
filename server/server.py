3f72b70c196fe0767ed15c86ec7114faf32e89a9
flarphengarg <anders.erik.lie@gmail.com>
2020-08-19 12:58:10 -0500

    [quick push]

--- server/server.py
+++ server/server.py
@@ -1,18 +1,42 @@
-from flask import Flask
+# === Libraries ===
+from flask import Flask, request
+import sys
 import os
+import mongoengine
+import bcrypt
 
-# == This section is necessary for deployment to work with the React build ==
 app = Flask(__name__, static_folder='../client/build', static_url_path='/')
-#app = Flask(__name__, static_folder='/app')
+with open(os.path.dirname(os.path.realpath(sys.argv[0])) + "\\" + 'mongodb.key') as f:
+    MONGODB_KEY = f.read()
 
-@app.route('/')
+db_name = 'groceries'
+mongoengine.connect(db='groceries', host="mongodb+srv://flarphengarg:{}@freebie-kgbod.gcp.mongodb.net/?retryWrites=true&w=majority".format(MONGODB_KEY))
+
+class User(mongoengine.Document):
+    meta = {'collection': 'users'}
+    name = mongoengine.StringField(required=True, unique=True)
+    password = mongoengine.StringField(required=True)
+
+def authenticate_user(provided_password, stored_password):
+    return bcrypt.checkpw(provided_password.encode(), stored_password.encode())
+
+
+# Frontend build redirect
+@app.route('/', methods=['GET'])
 def index():
     return app.send_static_file('index.html')
-# ===========================================================================
 
-@app.route('/api/hello')
-def hello():
-    return 'Hello from Flask!'
+@app.route('/api/users', methods=['POST'])
+def create_user():
+    req = request.get_json()
+
+    hashed_password = bcrypt.hashpw(req['password'].encode(), bcrypt.gensalt()).decode()
+    new_user = User(name=req['name'], password=hashed_password)
+    try:
+        new_user.save()
+    except mongoengine.errors.NotUniqueError:
+        return "Username already exists.", 400
+    return "OK"
 
 if __name__ == '__main__':
     try:
