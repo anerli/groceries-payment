2c9cf6badb781b80445d484a5afce14629057714
flarphengarg <anders.erik.lie@gmail.com>
2020-08-31 20:45:04 -0500

    get mongodb key from environment variable if key file not found:

--- server/lib/database.py
+++ server/lib/database.py
@@ -2,8 +2,12 @@ from pymongo import MongoClient
 import os, sys
 
 __all__ = ['db']
-with open(os.path.dirname(os.path.realpath(__file__)) + "/" + 'mongodb.key') as f:
-    MONGODB_KEY = f.read()
+try:
+    with open(os.path.dirname(os.path.realpath(__file__)) + "/" + 'mongodb.key') as f:
+        MONGODB_KEY = f.read()
+except:
+    # file not found, like if heroku is running it (bc key is on gitignore)
+    MONGODB_KEY = os.environ['MONGODB_KEY']
 
 client = MongoClient("mongodb+srv://flarphengarg:{}@freebie-kgbod.gcp.mongodb.net/test?retryWrites=true&w=majority".format(MONGODB_KEY))
 db = client.groceries
