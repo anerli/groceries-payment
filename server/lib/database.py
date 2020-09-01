df8583d375c05e4aa8238b70bbef3ab72d721984
flarphengarg <anders.erik.lie@gmail.com>
2020-08-31 20:28:28 -0500

    directory fix

--- server/lib/database.py
+++ server/lib/database.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 import os, sys
 
 __all__ = ['db']
-with open(os.path.dirname(os.path.realpath(__file__)) + "\\" + 'mongodb.key') as f:
+with open(os.path.dirname(os.path.realpath(__file__)) + "/" + 'mongodb.key') as f:
     MONGODB_KEY = f.read()
 
 client = MongoClient("mongodb+srv://flarphengarg:{}@freebie-kgbod.gcp.mongodb.net/test?retryWrites=true&w=majority".format(MONGODB_KEY))
